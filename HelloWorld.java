ab40c5e02ff344de0e6d16b9a891f70254b02701
simoncarlstedt <simon.carlstedt@gmail.com>
2016-06-08 15:49:42 +0100

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,30 @@
+import java.util.logging.Logger;
+
+//import org.ujmp.core.DenseMatrix;
+//import org.ujmp.core.Matrix;
+//import org.ujmp.core.SparseMatrix;
+//import org.ujmp.core.calculation.Calculation.Ret;
+//import org.ujmp.core.intmatrix.impl.ImageMatrix;
+
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Simon Again!");
+		int x = 3;	
+		double y = 1;
+		
+		System.out.println("testt");
+		
+		while (x < 5) {
+			System.out.println(x);
+			x++;
+		}
+		
+		System.out.println(x*y);
+		
+		//Logger logger = LoggerFactory.getLogger(App.class);
+		//logger.info("Hello World m2e");
+	}
+}
